7f12be52bc5f4a4d75236fcaa571d23711a211c5
Thà Lê <43161363+Hknight-official@users.noreply.github.com>
2020-11-17 22:56:27 +0700

    Update done.php

--- ajax/done.php
+++ ajax/done.php
@@ -2,7 +2,10 @@
 include(__DIR__ ."/../api/config.php");
 
 if($_POST["captcha"] != $_SESSION["captcha_code"]){
-	exit('<script type="text/javascript">toastr.error("Xác nhận captcha không đúng !");</script>');
+    exit(json_encode([
+        "status" => 0,
+        "msg" => "Xác nhận captcha không đúng !"
+    ]));
 }
 sleep(5);
 $username = $conn->real_escape_string(strip_tags(addslashes($_POST['username']))); 
@@ -13,7 +16,13 @@ $content = $conn->real_escape_string(strip_tags(addslashes($_POST['content'])));
 
 $query = $conn->query("SELECT * FROM `trans_log` WHERE `username` = '{$username}' AND `sdtnhan` = '{$sdtnhan}' AND `status` = 1 AND `name_nhan` = '{$name_nhan}' AND `ghichu` = '{$key_content}' AND `trans_id` = '{$content}'");
 if (!$query->num_rows > 0){
-    exit('<script type="text/javascript">toastr.error("Bạn chưa thanh toán !");</script>');
+    exit(json_encode([
+        "status" => 0,
+        "msg" => "Bạn chưa thanh toán !"
+    ]));
 }
 $row = $query->fetch_assoc();
-exit('<script type="text/javascript">swal("Thành Công", "Thanh Toán Thành Công '.number_format($row['amount']).'đ ", "success");</script>');
+exit(json_encode([
+    "status" => 1,
+    "msg" => "Thanh Toán Thành Công ".number_format($row['amount'])."đ "
+]));
